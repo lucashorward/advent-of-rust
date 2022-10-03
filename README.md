14dae65a67b4fa236b07662a53eb6c60975591cf
Lucas Horward <l.horward@essent.nl>
2022-10-03 20:17:13 +0200

    Basic setup & day 1

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a small project where I do some of the advent-of-code puzzles as listed on https://adventofcode.com/, in Rust!
---
+++ day1/src/main.rs
@@ -0,0 +1,53 @@
+#[derive(Debug, PartialEq)]
+enum DepthChange {
+    None,
+    Increased,
+    Decreased,
+}
+
+fn diff_to_depth(left: i32, right: i32) -> DepthChange {
+    let diff = left - right;
+    if diff == 0 {
+        DepthChange::None
+    } else if diff > 0 {
+        DepthChange::Decreased
+    } else {
+        DepthChange::Increased
+    }
+}
+
+fn sonar_sweep(diffs: Vec<i32>) -> i32 {
+    let mut i = 0;
+    let mut results:Vec<DepthChange> = vec![];
+    while i < diffs.len() -1 {
+        let current = diffs[i];
+        let next = diffs[i + 1];
+        let result = diff_to_depth(current, next);
+        println!("{} to {} is a change of: {:?}", current, next, result);
+        results.push(result);
+        i += 1;
+    }
+    let amount_increased: usize = results.iter().filter(|&n| *n == DepthChange::Increased).count();
+    print!("{} change(s) were an increase!\n", amount_increased);
+    return amount_increased as i32;
+}
+
+fn main() {
+    // The numbers as provided in the advent-of-code day 1 description
+    sonar_sweep(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_3_numbers_inc() {
+        assert_eq!(sonar_sweep(vec![1, 2, 3]), 2);
+    }
+
+    #[test]
+    fn test_3_numbers_dec() {
+        assert_eq!(sonar_sweep(vec![3, 2, 1]), 0);
+    }
+}
